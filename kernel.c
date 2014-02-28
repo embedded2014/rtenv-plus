b715ce7e1cbe2cdfcaed4308e2ea09210673ec6f
southernbear <xq5530303@gmail.com>
2014-02-28 14:38:31 +0800

    move pathserver to path.h and path.c

--- kernel.c
+++ kernel.c
@@ -11,6 +11,7 @@
 #include "string.h"
 #include "task.h"
 #include "memory-pool.h"
+#include "path.h"
 #include "pipe.h"
 #include "fifo.h"
 #include "mqueue.h"
@@ -82,58 +83,6 @@ int env_count = 0;
 
 struct task_control_block tasks[TASK_LIMIT];
 
-/* 
- * pathserver assumes that all files are FIFOs that were registered
- * with mkfifo.  It also assumes a global tables of FDs shared by all
- * processes.  It would have to get much smarter to be generally useful.
- *
- * The first TASK_LIMIT FDs are reserved for use by their respective tasks.
- * 0-2 are reserved FDs and are skipped.
- * The server registers itself at /sys/pathserver
-*/
-#define PATH_SERVER_NAME "/sys/pathserver"
-void pathserver()
-{
-	char paths[PIPE_LIMIT - TASK_LIMIT - 3][PATH_MAX];
-	int npaths = 0;
-	int i = 0;
-	unsigned int plen = 0;
-	unsigned int replyfd = 0;
-	char path[PATH_MAX];
-
-	memcpy(paths[npaths++], PATH_SERVER_NAME, sizeof(PATH_SERVER_NAME));
-
-	while (1) {
-		read(PATHSERVER_FD, &replyfd, 4);
-		read(PATHSERVER_FD, &plen, 4);
-		read(PATHSERVER_FD, path, plen);
-
-		if (!replyfd) { /* mkfifo */
-			int dev;
-			read(PATHSERVER_FD, &dev, 4);
-			memcpy(paths[npaths], path, plen);
-			mknod(npaths + 3 + TASK_LIMIT, 0, dev);
-			npaths++;
-		}
-		else { /* open */
-			/* Search for path */
-			for (i = 0; i < npaths; i++) {
-				if (*paths[i] && strcmp(path, paths[i]) == 0) {
-					i += 3; /* 0-2 are reserved */
-					i += TASK_LIMIT; /* FDs reserved for tasks */
-					write(replyfd, &i, 4);
-					i = 0;
-					break;
-				}
-			}
-
-			if (i >= npaths) {
-				i = -1; /* Error: not found */
-				write(replyfd, &i, 4);
-			}
-		}
-	}
-}
 
 void serialout(USART_TypeDef* uart, unsigned int intr)
 {
---
+++ path.h
@@ -0,0 +1,8 @@
+#ifndef PATH_H
+#define PATH_H
+
+#define PATH_SERVER_NAME "/sys/pathserver"
+
+void pathserver();
+
+#endif
