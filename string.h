050cb1c146a7d4568633ad71e0e33a144dd02c7b
southernbear <xq5530303@gmail.com>
2014-03-02 00:09:10 +0800

    fix implicit-function-declaration of strcpy and strncmp

--- string.h
+++ string.h
@@ -6,5 +6,7 @@
 void *memcpy(void *dest, const void *src, size_t n);
 int strcmp(const char *a, const char *b) __attribute__ ((naked));
 size_t strlen(const char *s) __attribute__ ((naked));
+char *strcpy(char *dest, const char *src);
+int strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
