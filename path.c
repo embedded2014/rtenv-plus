60ab845ade8ba87e498f34edd2e6cec67711fccc
southernbear <xq5530303@gmail.com>
2014-03-04 18:59:35 +0800

    New protocol for path server in order to add more functions

--- path.c
+++ path.c
@@ -18,48 +18,66 @@ void pathserver()
 	char paths[PIPE_LIMIT - TASK_LIMIT - 3][PATH_MAX];
 	int npaths = 0;
 	int i = 0;
+	int cmd = 0;
 	unsigned int plen = 0;
 	unsigned int replyfd = 0;
 	char path[PATH_MAX];
+	int dev = 0;
+	int newfd = 0;
 
 	memcpy(paths[npaths++], PATH_SERVER_NAME, sizeof(PATH_SERVER_NAME));
 
 	while (1) {
+		read(PATHSERVER_FD, &cmd, 4);
 		read(PATHSERVER_FD, &replyfd, 4);
-		read(PATHSERVER_FD, &plen, 4);
-		read(PATHSERVER_FD, path, plen);
 
-		if (!replyfd) { /* mkfile or path_register */
-			int dev;
-			read(PATHSERVER_FD, &dev, 4);
-			memcpy(paths[npaths], path, plen);
-			if (dev == 0) { /* path_register */
-			    int fd = npaths + 3 + TASK_LIMIT;
-			    int reg_replyfd;
-			    read(PATHSERVER_FD, &reg_replyfd, 4);
-				write(reg_replyfd, &fd, 4);
-			}
-			else { /* mkfile */
-			    mknod(npaths + 3 + TASK_LIMIT, 0, dev);
-			}
-			npaths++;
-		}
-		else { /* open */
-			/* Search for path */
-			for (i = 0; i < npaths; i++) {
-				if (*paths[i] && strcmp(path, paths[i]) == 0) {
-					i += 3; /* 0-2 are reserved */
-					i += TASK_LIMIT; /* FDs reserved for tasks */
-					write(replyfd, &i, 4);
-					i = 0;
-					break;
-				}
-			}
+		switch (cmd) {
+		    case PATH_CMD_MKFILE:
+		        read(PATHSERVER_FD, &plen, 4);
+		        read(PATHSERVER_FD, path, plen);
+			    read(PATHSERVER_FD, &dev, 4);
+			    newfd = npaths + 3 + TASK_LIMIT;
+			    if (mknod(newfd, 0, dev) == 0) {
+			        memcpy(paths[npaths], path, plen);
+			        npaths++;
+			    }
+			    else {
+			        newfd = -1;
+			    }
+			    write(replyfd, &newfd, 4);
+		        break;
 
-			if (i >= npaths) {
-				i = -1; /* Error: not found */
-				write(replyfd, &i, 4);
-			}
+		    case PATH_CMD_OPEN:
+		        read(PATHSERVER_FD, &plen, 4);
+		        read(PATHSERVER_FD, path, plen);
+		        /* Search for path */
+			    for (i = 0; i < npaths; i++) {
+				    if (*paths[i] && strcmp(path, paths[i]) == 0) {
+					    i += 3; /* 0-2 are reserved */
+					    i += TASK_LIMIT; /* FDs reserved for tasks */
+					    write(replyfd, &i, 4);
+					    i = 0;
+					    break;
+				    }
+			    }
+
+			    if (i >= npaths) {
+				    i = -1; /* Error: not found */
+				    write(replyfd, &i, 4);
+			    }
+		        break;
+
+		    case PATH_CMD_REGISTER_PATH:
+		        read(PATHSERVER_FD, &plen, 4);
+		        read(PATHSERVER_FD, path, plen);
+			    newfd = npaths + 3 + TASK_LIMIT;
+			    memcpy(paths[npaths], path, plen);
+		        npaths++;
+				write(replyfd, &newfd, 4);
+		        break;
+
+		    default:
+		        ;
 		}
 	}
 }
--- path.h
+++ path.h
@@ -3,6 +3,10 @@
 
 #define PATH_SERVER_NAME "/sys/pathserver"
 
+#define PATH_CMD_MKFILE 1
+#define PATH_CMD_OPEN 2
+#define PATH_CMD_REGISTER_PATH 3
+
 void pathserver();
 int path_register(const char *pathname);
 
