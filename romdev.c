a592eba3b65adae067a5c0657a5bc403af355e4a
southernbear <xq5530303@gmail.com>
2014-03-06 12:34:32 +0800

    romdev implement seek function

--- romdev.c
+++ romdev.c
@@ -29,28 +29,48 @@ void romdev_driver()
 	    if (read(self, &request, sizeof(request)) == sizeof(request)) {
 	        cmd = request.cmd;
 
-	        if (cmd == BLOCK_CMD_READ) {
-	            fd = request.fd;
-	            size = request.size;
-	            pos = request.pos;
+	        switch (cmd) {
+	            case BLOCK_CMD_READ:
+	                fd = request.fd;
+	                size = request.size;
+	                pos = request.pos;
 
-                /* Check boundary */
-                request_start = &_sromdev + pos;
-                if (request_start < &_sromdev)
+                    /* Check boundary */
+                    request_start = &_sromdev + pos;
+                    if (request_start < &_sromdev)
+	                    block_response(fd, NULL, -1);
+                    if (request_start > &_eromdev)
+                        request_start = &_eromdev;
+
+                    request_end = request_start + size;
+                    if (request_end > &_eromdev)
+                        request_end = &_eromdev;
+
+                    /* Response */
+                    request_len = request_end - request_start;
+	                block_response(fd, (char *)request_start, request_len);
+	                break;
+
+	            case BLOCK_CMD_SEEK:
+	                fd = request.fd;
+	                size = request.size;
+	                pos = request.pos;
+
+	                if (pos == 0) { /* SEEK_SET */
+	                    request_len = size;
+	                }
+	                else if (pos < 0) { /* SEEK_END */
+	                    request_len = (&_eromdev - &_sromdev) + size;
+	                }
+	                else { /* SEEK_CUR */
+	                    request_len = pos + size;
+	                }
+	                lseek(fd, request_len, SEEK_SET);
+	                break;
+
+	            case BLOCK_CMD_WRITE: /* readonly */
+	            default:
 	                block_response(fd, NULL, -1);
-                if (request_start > &_eromdev)
-                    request_start = &_eromdev;
-
-                request_end = request_start + size;
-                if (request_end > &_eromdev)
-                    request_end = &_eromdev;
-
-                /* Response */
-                request_len = request_end - request_start;
-	            block_response(fd, (char *)request_start, request_len);
-	        }
-	        else { /* readonly */
-	            block_response(fd, NULL, -1);
 	        }
 	    }
 	}
